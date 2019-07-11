019cac1868564e2a1f73c8a4289b1072023111eb
Dillon <18711345569@163.com>
2019-07-11 23:40:29 +0800

    finish some practice

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,18 +2,52 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CollectionOperator {
     public List<Integer> getListByInterval(int left, int right) {
-        throw new NotImplementedException();
+        List<Integer> list=new ArrayList<>();
+        if(left<right){
+            for (int i=left;i<=right;i++){
+                list.add(i);
+            }
+        }else{
+            for (int i=left;i>=right;i--){
+                list.add(i);
+            }
+        }
+        return list;
+
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
-        throw new NotImplementedException();
+        List<Integer> list=new ArrayList<>();
+        if (left<right){
+            for (int i=left;i<=right;i++){
+                if(i%2==0){
+                    list.add(i);
+                }
+            }
+        }else {
+            for (int i=left;i>=right;i--){
+                if(i%2==0){
+                    list.add(i);
+                }
+            }
+        }
+        return list;
     }
 
     public List<Integer> popEvenElments(int[] array) {
+//        int[] array2=new int[array.length];
+//        for (int i=0,j=0;i<array.length;i++){
+//            if (array[i]%2==0){
+//                array2[j]=array[i];
+//                j++;
+//            }
+//        }
+//        return array2;
         throw new NotImplementedException();
     }
 
