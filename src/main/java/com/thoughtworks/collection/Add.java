89ad4fc3836678cc83d4acc0cb48c3380e8a022c
Dillon <18711345569@163.com>
2019-07-11 23:10:25 +0800

    finish add.file

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -2,23 +2,30 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Add {
     public int getSumOfEvens(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        int max = Math.max(leftBorder,rightBorder);
+        int min = Math.min(leftBorder,rightBorder);
+        int sum = IntStream.rangeClosed(min,max).filter(num->num%2==0).sum();
+        return sum;
     }
 
     public int getSumOfOdds(int leftBorder, int rightBorder) {
         throw new NotImplementedException();
     }
 
-    public int getSumTripleAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+    public long getSumTripleAndAddTwo(List<Integer> arrayList) {
+        IntSummaryStatistics stats = arrayList.stream().mapToInt((x) ->3*x+2).summaryStatistics();
+        return stats.getSum();
     }
 
     public List<Integer> getTripleOfOddAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return arrayList.stream().map(x -> x%2==0?x:x*3+2).collect(Collectors.toList());
     }
 
     public int getSumOfProcessedOdds(List<Integer> arrayList) {
@@ -30,7 +37,7 @@ public class Add {
 //    }
 
     public double getAverageOfEven(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return arrayList.stream().filter(x->x%2==0).collect(Collectors.averagingDouble(x -> x));
     }
 
     public boolean isIncludedInEvenIndex(List<Integer> arrayList, Integer specialElment) {
@@ -38,7 +45,7 @@ public class Add {
     }
 
     public List<Integer> getUnrepeatedFromEvenIndex(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return arrayList.stream().filter(x->x%2==0).distinct().collect(Collectors.toList());
     }
 
 //    public List<Integer> sortByEvenAndOdd(List<Integer> arrayList) {
